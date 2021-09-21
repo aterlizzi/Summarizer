4af13d91d08d4cb7c413cca4d6213dd0146f5081
Aidan Terlizzi <act5@rice.edu>
2021-09-20 23:22:47 -0500

    basic server setup w/ graphql & postgres

---
+++ server/src/entities/User.ts
@@ -0,0 +1,24 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import { User } from "./entities/User";
+import { buildSchema } from "type-graphql";
+import dotenv from "dotenv";
+import "reflect-metadata";
+dotenv.config();
+
+const fastify = require("fastify");
+import mercurius from "mercurius";
+import { createConnection } from "typeorm";
+
+const main = async () => {
+  const app = fastify();
+  await createConnection({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "admin",
+    password: "admin",
+    database: "summarizer",
+    entities: [User],
+    synchronize: true,
+    logging: process.env.NODE_ENV !== "production",
+  });
+  const schema = await buildSchema({
+    resolvers: [__dirname + "/resolvers/*/*.{ts,js}"],
+    validate: true,
+  });
+  app.register(mercurius, {
+    schema,
+    graphiql: true,
+  });
+  app.get("/", async function (_: any, reply: any) {
+    const query = "{ add(x: 2, y: 2) }";
+    return reply.graphql(query);
+  });
+  app.listen(parseInt(process.env.PORT!), () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+};
+main().catch((err) => console.log(err));
