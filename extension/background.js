eaff20e100a8127bec384d43bc89feacf79b8030
Aidan Terlizzi <act5@rice.edu>
2021-09-21 18:10:49 -0500

    extension login partial complete

--- extension/background.js
+++ extension/background.js
@@ -4,38 +4,37 @@ let userSignedIn = false;
 
 const verifyUserStatus = () => {
   return new Promise((resolve) => {
-    chrome.storage.local.get(["userStatus", "userInfo"], (response) => {});
-    if (chrome.runtime.lastError) resolve({ userStatus: false, userInfo: {} });
-    resolve(
-      response.userStatus === undefined
-        ? { userStatus: false, userInfo: {} }
-        : { userStatus: response.userStatus, userInfo: response.userInfo }
-    );
+    chrome.storage.local.get(["userStatus", "userInfo"], (response) => {
+      if (chrome.runtime.lastError) {
+        resolve({ userStatus: false, userInfo: {} });
+      }
+      resolve(
+        response.userStatus === undefined
+          ? { userStatus: false, userInfo: {} }
+          : { userStatus: response.userStatus, userInfo: response.userInfo }
+      );
+    });
   });
 };
 chrome.runtime.onMessage.addListener(receiver);
-chrome.action.onClicked.addListener(() => {
-  verifyUserStatus()
-    .then((res) => {
-      if (res.userStatus) returnSession = true;
-      userSignedIn = res.userStatus;
-    })
-    .catch((err) => console.log(err));
-  switch (userSignedIn) {
-    case true:
-      break;
-    case false:
-      chrome.action.setPopup({ popup: "./signIn.html" });
-      break;
-    default:
-      break;
-  }
-});
 
 function receiver(req, sender, sendResponse) {
   switch (req.key) {
     case "status":
-      // verify logged in status
+      verifyUserStatus().then((res) => {
+        if (res.userStatus) returnSession = true;
+        userSignedIn = res.userStatus;
+        switch (userSignedIn) {
+          case true:
+            sendResponse({ key: "loginTrue" });
+            break;
+          case false:
+            sendResponse({ key: "loginFalse" });
+            break;
+          default:
+            break;
+        }
+      });
       break;
     case "login":
       // verify user and log user in
@@ -49,6 +48,7 @@ function receiver(req, sender, sendResponse) {
     default:
       break;
   }
+  return true;
 }
 // const data = {
 //   prompt: `Write a short summary of the text.\nText: ${req.text}\nSummary:`,
--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,22 @@
 const emptyTag = document.querySelector(".summary");
 const button = document.querySelector(".btn");
 const spinner = document.querySelector(".hidden");
+const sumWrapper = document.querySelector(".sumWrapper");
+const mainSpinnerContainer = document.querySelector(".mainSpinnerContainer");
+const loginWrapper = document.querySelector(".loginWrapper");
 
-chrome.browserAction.onClicked.addListener();
+// get status check when loading the popup
+window.onload = () => {
+  console.log("ready");
+  chrome.runtime.sendMessage({ key: "status" }, (response) => {
+    if (response.key === "loginTrue") {
+      sumWrapper.classList.remove("none");
+    } else if (response.key === "loginFalse") {
+      loginWrapper.classList.remove("none");
+    }
+    mainSpinnerContainer.classList.add("none");
+  });
+};
 
 button.addEventListener("click", () => {
   button.classList.toggle("hidden");
@@ -11,17 +25,23 @@ button.addEventListener("click", () => {
     chrome.tabs.sendMessage(tabs[0].id, { text: "Hello world." });
   });
 });
+
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
   console.log(msg);
   let summary;
-  if (msg.key === "k8k4IQwFaX") {
-    button.classList.toggle("hidden");
-    spinner.classList.toggle("hidden");
-    summary = msg.text;
-  } else if (msg.key === "ogLlRDalkA") {
-    button.classList.toggle("hidden");
-    spinner.classList.toggle("hidden");
-    summary = msg.text;
+  switch (msg.key) {
+    case "k8k4IQwFaX":
+      button.classList.toggle("hidden");
+      spinner.classList.toggle("hidden");
+      summary = msg.text;
+      emptyTag.textContent = summary;
+      break;
+    case "ogLlRDalkA":
+      button.classList.toggle("hidden");
+      spinner.classList.toggle("hidden");
+      summary = msg.text;
+      break;
+    default:
+      break;
   }
-  emptyTag.textContent = summary;
 });
