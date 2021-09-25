65b12197f2d7950b85de7867729bcb42ecdfbb34
Aidan Terlizzi <act5@rice.edu>
2021-09-25 15:27:22 -0500

    backend changes

---
+++ server/src/resolvers/openai/summarize.ts
@@ -0,0 +1,23 @@
+import { User } from "../../entities/User";
+import { Arg, Mutation, Resolver } from "type-graphql";
+const OpenAI = require("openai-api");
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+@Resolver()
+export class SummarizeResolver {
+  @Mutation(() => String)
+  async summarize(
+    @Arg("email", { nullable: true }) email: string,
+    @Arg("sub", { nullable: true }) sub: string,
+    @Arg("content") content: string
+  ): Promise<string> {
+    let user;
+    if (email) {
+      user = await User.findOne({ where: { email } });
+    } else if (sub) {
+      user = await User.findOne({ where: { googleSubKey: sub } });
+    }
+    if (!user) return "";
+    return content;
+  }
+}
