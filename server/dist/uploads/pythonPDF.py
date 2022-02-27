7d63c631e459cccc89fec9a16ee992bfb6c2bacc
Aidan Terlizzi <act5@rice.edu>
2022-02-27 16:04:57 -0600

    changed python script

--- server/dist/uploads/pythonPDF.py
+++ server/dist/uploads/pythonPDF.py
@@ -1,26 +1,26 @@
-import os
-import sys
-import pdfminer.high_level
-import ocrmypdf
-import re
+try:
+    import sys
+    import pdfminer.high_level
+    import ocrmypdf
+    import re
 
+    # initial arguments
+    path_to_pdf = sys.argv[1]
+    filename = sys.argv[2]
 
-# initial arguments
-path_to_pdf = sys.argv[1]
-filename = sys.argv[2]
+    # non-ocr procedure
+    text = pdfminer.high_level.extract_text(path_to_pdf)
 
-# non-ocr procedure
-text = pdfminer.high_level.extract_text(path_to_pdf)
+    # check to see if ocr is necessary
+    if not re.search("a", text):
+        if __name__ == '__main__':
+            ocrmypdf.ocr(path_to_pdf, path_to_pdf, deskew=True)
 
-# check to see if ocr is necessary
-if not re.search("a", text):
-    if __name__ == '__main__':
-        ocrmypdf.ocr(path_to_pdf, path_to_pdf, deskew=True)
-
-# extract text again
-text = pdfminer.high_level.extract_text(path_to_pdf)
-print(text)
-
+    # extract text again
+    text = pdfminer.high_level.extract_text(path_to_pdf)
+    print(text)
+except Exception as e:
+    print(e)
 
 
 
