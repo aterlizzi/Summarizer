f39077d28e46d293c378009ec5859f1438451fa9
Aidan Terlizzi <act5@rice.edu>
2022-03-19 12:48:36 -0400

    testing email on prod

--- server/dist/utils/emails/changeEmail/changedEmailToOwner.js
+++ server/dist/utils/emails/changeEmail/changedEmailToOwner.js
@@ -45,39 +45,109 @@ Rest assured, as long as your password is not jeopardized, your email cannot be 
         
 Best, 
 The Untanglify Team`,
-                    html: `
-        <main style="min-height: 100vh; width: 100%; background: #121212; display: flex; flex-direction: column; align-items: center; padding: 2em;">
-          <div style="display: flex; align-items: center;">
-            <p style="color: rgba(255, 255, 255, 0.87); font-size: 2rem;">
-              Untanglify
-            </p>
-          </div>
-          <div style="background: rgba(255, 255, 255, 0.05); border-radius: 2em; max-width: 30em; width: 90%; border: "1px solid rgba(255, 255, 255, 0.08);">
-            <header style="background: #4740d1; border-bottom: 3px solid rgba(255, 255, 255, 0.08); padding: 2em 1em; border-top-left-radius: 2em; border-top-right-radius: 2em;">
-              <h3 style="color: rgba(255, 255, 255, 0.87); margin: 0em;">
-                We Got A Request To Change Your Email
-              </h3>
-            </header>
-            <section style="padding: 1em; display: flex; flex-direction: column;">
-              <p style="color: rgba(255, 255, 255, .6); margin: 0em">
-                Hi ${username}, 
-                <br /> <br />
-                We got a request to change the email associated with your Untanglify account.
-                <br /> <br />
-                If this was you, you can ignore this email.
-                <br /> <br />
-                If this wasn't you, and you fear your account might be in jeopardy, please contact
-                <a href="https://untanglify.com/contact" style="text-decoration: none;">
-                  <span style="color: #bb86fc;">Customer Support.</span>
-                </a>
-                <br /> <br />
-                Best,
-                <br />
-                The Untanglify Team.
-              </p>
-            </section>
-          </div>
-        </main>`,
+                    html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Structure Untanglify</title>
+            <style type="text/css">
+              @media screen and (max-width: 600px) {
+                    }
+                    @media screen and (max-width: 400px) {
+                    }
+            </style>
+          </head>
+          <body style="margin:0;padding:0;background:#121212;">
+            <center class="wrapper" style="width:100%;table-layout:fixed;background:#121212;padding-bottom:40px;">
+              <div class="webkit" style="max-width:600px;">
+                <table class="outer" align="center" style="Margin:0 auto;max-width:600px;width:100%;border-spacing:0;font-family:sans-serif;color:#ffffff99;">
+                  <tr>
+                    <td style="padding-top:20px;padding-bottom:40px;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td width="100%" style="padding:0;text-align:center;">
+                            <h3 style="display:inline-block;color:#ffffffde;Margin:0;font-size:45px;">Untanglify</h3>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="padding: 20px;background:#4740d1;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td style="padding:0;font-size:25px;color:#ffffffde;">
+                            <h4 style="Margin:0;">
+                              We Got A Request To Change Your Email
+                            </h4>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="padding:0;background:#564ee2;" width="100%" height="5"></td>
+                  </tr>
+                  <tr>
+                    <td style="padding:0;background:#ffffff0d;padding: 20px;color: #ffffff99;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              Hi ${username},
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              We got a request to change the email associated with your Untanglify
+                              account.
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              If this was you, you can ignore this email.
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              If this wasn't you, and you fear your account might be in jeopardy,
+                              please contact
+                              <a href="https://untanglify.com/contact" style="text-decoration: none">
+                                <span style="color: #bb86fc">Customer Support.</span>
+                              </a>
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding:0px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              Best,
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              The Untanglify Team.
+                            </p>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </center>
+          </body>
+        </html>`,
                 };
                 let info = yield transport.sendMail(mailOptions);
                 console.log("Message sent: %s", info.messageId);
@@ -110,39 +180,109 @@ Rest assured, as long as your password is not jeopardized, your email cannot be 
         
 Best, 
 The Untanglify Team`,
-                    html: `
-        <main style="min-height: 100vh; width: 100%; background: #121212; display: flex; flex-direction: column; align-items: center; padding: 2em;">
-          <div style="display: flex; align-items: center;">
-            <p style="color: rgba(255, 255, 255, 0.87); font-size: 2rem;">
-              Untanglify
-            </p>
-          </div>
-          <div style="background: rgba(255, 255, 255, 0.05); border-radius: 2em; max-width: 30em; width: 90%; border: "1px solid rgba(255, 255, 255, 0.08);">
-            <header style="background: #4740d1; border-bottom: 3px solid rgba(255, 255, 255, 0.08); padding: 2em 1em; border-top-left-radius: 2em; border-top-right-radius: 2em;">
-              <h3 style="color: rgba(255, 255, 255, 0.87); margin: 0em;">
-                We Got A Request To Change Your Email
-              </h3>
-            </header>
-            <section style="padding: 1em; display: flex; flex-direction: column;">
-              <p style="color: rgba(255, 255, 255, .6); margin: 0em 0em 1em 0em">
-                Hi ${username}, 
-                <br /> <br />
-                We got a request to change the email associated with your Untanglify account.
-                <br /> <br />
-                If this was you, you can ignore this email.
-                <br /> <br />
-                If this wasn't you, and you fear your account might be in jeopardy, please contact
-                <a href="https://untanglify.com/contact" style="text-decoration: none;">
-                  <span style="color: #bb86fc;">Customer Support.</span>
-                </a>
-                <br /> <br />
-                Best,
-                <br />
-                The Untanglify Team.
-              </p>
-            </section>
-          </div>
-        </main>`,
+                    html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Structure Untanglify</title>
+            <style type="text/css">
+              @media screen and (max-width: 600px) {
+                    }
+                    @media screen and (max-width: 400px) {
+                    }
+            </style>
+          </head>
+          <body style="margin:0;padding:0;background:#121212;">
+            <center class="wrapper" style="width:100%;table-layout:fixed;background:#121212;padding-bottom:40px;">
+              <div class="webkit" style="max-width:600px;">
+                <table class="outer" align="center" style="Margin:0 auto;max-width:600px;width:100%;border-spacing:0;font-family:sans-serif;color:#ffffff99;">
+                  <tr>
+                    <td style="padding-top:20px;padding-bottom:40px;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td width="100%" style="padding:0;text-align:center;">
+                            <h3 style="display:inline-block;color:#ffffffde;Margin:0;font-size:45px;">Untanglify</h3>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="padding: 20px;background:#4740d1;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td style="padding:0;font-size:25px;color:#ffffffde;">
+                            <h4 style="Margin:0;">
+                              We Got A Request To Change Your Email
+                            </h4>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="padding:0;background:#564ee2;" width="100%" height="5"></td>
+                  </tr>
+                  <tr>
+                    <td style="padding:0;background:#ffffff0d;padding: 20px;color: #ffffff99;">
+                      <table width="100%" style="border-spacing:0;">
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              Hi ${username},
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              We got a request to change the email associated with your Untanglify
+                              account.
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              If this was you, you can ignore this email.
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              If this wasn't you, and you fear your account might be in jeopardy,
+                              please contact
+                              <a href="https://untanglify.com/contact" style="text-decoration: none">
+                                <span style="color: #bb86fc">Customer Support.</span>
+                              </a>
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding:0px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              Best,
+                            </p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-bottom:10px;color: #ffffff99;">
+                            <p style="Margin:0;">
+                              The Untanglify Team.
+                            </p>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </center>
+          </body>
+        </html>`,
                 };
                 let info = yield transporter.sendMail(mailOptions);
                 console.log("Message sent: %s", info.messageId);
